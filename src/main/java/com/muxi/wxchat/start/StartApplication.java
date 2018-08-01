21569922416f3fde6bdcdb4a6c2ee5d8e400b7eb
Rookie259 <13213731716@163.com>
2018-08-01 21:11:48 +0800

    配置在tomcat中运行

--- src/main/java/com/muxi/wxchat/start/StartApplication.java
+++ src/main/java/com/muxi/wxchat/start/StartApplication.java
@@ -4,6 +4,8 @@ package com.muxi.wxchat.start;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.muxi.wxchat.services.*","com.muxi.wxchat.function","com.muxi.wxchat.controller","com.muxi.wxchat.quatrz"})
 @MapperScan({"com.muxi.wxchat.dao"})
-public class StartApplication {
+public class StartApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StartApplication.class);
+    }
 
     public static void main(String[] args) {
 
